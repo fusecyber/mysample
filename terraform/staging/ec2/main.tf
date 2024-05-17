8a8615ada3aa7a48fdb08c5bd28dd376d2b4e6e5
fusecyber <fusecyber@gmail.com>
2024-05-17 19:08:56 +0900

    lint

--- terraform/staging/ec2/main.tf
+++ terraform/staging/ec2/main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "example_server" {
   ami           = "ami-04e914639d0cca79a"
   instance_type = "t2.micro"
-  image_id      = var.image_id
 
   tags = {
-    Name = "JacksBlogExample"
+    Name = var.image_id
   }
 }
